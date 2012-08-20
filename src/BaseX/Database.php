c47065f75779fa3cb08103032f4fb224f8cb5e3b
Alexandros Sigalas <alxarch@gmail.com>
2012-08-20 12:23:18 +0300

    - Abstract Document & Database access with simple classes. [WIP]

---
+++ src/BaseX/Database.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace BaseX;
+
+use BaseX\Session;
+use BaseX\Document;
+
+class Database
+{
+    
+  /**
+   *
+   * @var \BaseX\Session
+   */
+  protected $session;
+  
+  /**
+   *
+   * @var string
+   */
+  protected $name;
+  
+  public function __construct(Session $session, $name)
+  {
+    $this->session = $session;
+    $this->name = $name;
+    
+    // Create the database if it does not exist.
+    $com = sprintf('CHECK %s', $name);
+    $this->session->execute($com);
+  }
+  
+  public function getName()
+  {
+    return $this->name;
+  }
+  
+  public function add($path, $input)
+  {
+    $path = implode('/', array($this->getName(), $path));
+    return $this->session->add($path, $input);
+  }
+  
+  public function store($path, $content)
+  {
+    $path = implode('/', array($this->getName(), $path));
+    $this->session->store($path, $content);
+  }
+  
+  public function retrieve($path)
+  {
+    return $this->execute('RETRIEVE %s', $path);
+  }
+  
+  public function execute($com)
+  {
+    $count = func_num_args();
+    
+    $args = func_get_args();
+    
+    $com = $args[0];
+    
+    if($count > 1)
+    {
+      $com = call_user_func_array('sprintf', $args);
+    }
+    
+    $com = sprintf('OPEN %s; %s;', $this->getName(), $com);
+    
+    return $this->session->execute($com);
+  }
+  
+  public function document($path, $class=null)
+  {
+    if(null !== $class && !is_subclass_of($class, 'Document'))
+    {
+      throw new \InvalidArgumentException('Invalid class for document.');
+    }
+    
+    return new $class($this, $path);
+  }
+  
+  public function delete($path)
+  {
+    $this->execute('DELETE %s', $path);
+  }
+  
+  public function rename($old, $new)
+  {
+    $this->execute('RENAME %s %s', $old, $new);
+  }
+  
+  
+  public function index($path = null)
+  {
+    $xq = <<<XQUERY
+      decalre variable \$db external;
+      declare variable \$path external := "";
+      db:list-details(\$db, \$path)
+XQUERY;
+    
+    return $this->session
+      ->query($xq)
+      ->bind('db', $this->getName(), 'xs:string')
+      ->bind('path', $path, 'xs:string')
+      ->execute();
+  }
+  
+}
---
+++ src/BaseX/Document.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace BaseX;
+
+use BaseX\Database;
+
+class Document
+{
+  /**
+  *
+  * @var string
+  */
+  protected $path;
+  
+  /**
+   * 
+   * @var \BaseX\Database
+   */
+  protected $db;
+
+  /**
+   *
+   * @var boolean
+   */
+  protected $raw = true;
+
+  /**
+   *
+   * @var string
+   */
+  protected $type;
+
+  /**
+   *
+   * @var DateTime
+   */
+  protected $updated = null;
+
+  /**
+   *
+   * @var integer
+   */
+  protected $size;
+  
+  /**
+   *
+   * @var string
+   */
+  protected $contents = null;
+  
+  /**
+   *
+   * @var \DOMDocument
+   */
+  protected $xml = null;
+
+
+  public function __construct(Database $db, $path = null)
+  {
+    $this->db = $db;
+    $this->path = $path;
+    
+    $this->info();
+  }
+  
+  public function getContents()
+  {
+    if(null === $this->contents)
+    {
+      $this->getDatabase()->retrieve($this->getPath());
+    }
+    
+    return $this->contents;
+  }
+  
+  public function save($as = null)
+  {
+    $dest = null === $as ? $this->getPath() : (string) $as;
+    
+    $success = $this->raw ?
+        $this->getDatabase()->add($dest, $this->getContents()) :
+        $this->getDatabase()->store($dest, $this->getContents());
+    
+    if($success)
+    {
+      $this->info();
+    }
+    
+    return $success;
+  }
+  
+  public function move($to)
+  {
+    return $this->getDatabase()->rename($this->getPath(), $to);
+  }
+  
+  public function delete()
+  {
+    return $this->getDatabase()->delete($this->getPath());
+  }
+  
+  public function getDatabase()
+  {
+    return $this->db;
+  }
+  
+  public function getPath()
+  {
+    return $this->path;
+  }
+  
+  public function exists()
+  {
+    return null !== $this->updated;
+  }
+  
+  /**
+   * Returns the contents of the document as XML.
+   * 
+   * @return \DOMDocument
+   */
+  public function getXML()
+  {
+    if(!$this->raw && null === $this->xml)
+    {
+      $this->xml = \DOMDocument::loadXML($this->getContents());
+    }
+    
+    return $this->xml;
+  }
+  
+  protected function info()
+  {
+    $resources = $this->db->index($path);
+    if($resources)
+    {
+      $info = $resources[0];
+      $this->type = $info['content-type'];
+      $this->raw = $info['raw'];
+      $this->size = isset($info['size']) ? (int) $info['size'] : null;
+      $this->updated = new \DateTime($info['updated']);
+    }
+  }
+}
